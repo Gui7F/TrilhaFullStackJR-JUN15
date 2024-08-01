da1e281b54283aea17cb01d04e5b3e57a15949c7
Guilherme Ribeiro <80535297+Gui7F@users.noreply.github.com>
2024-08-01 01:21:38 +0000

    Alterações na navbar instalação do bootstrap icons

--- Project_Manager/frontend/src/App.jsx
+++ Project_Manager/frontend/src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Home from './pages/home'
 import Navbar from "./components/navbar"
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 
 function App() {
 
--- Project_Manager/frontend/src/components/navbar.jsx
+++ Project_Manager/frontend/src/components/navbar.jsx
@@ -5,16 +5,21 @@ import { Link } from "react-router-dom";
 
 const navbar = () => {
   return (
-    <div className="flex ">
-        <nav>
+    <div className="text-white">
+        <nav className=" list-none flex flex-row justify-between">
             <div>
                 <img className="object-cover h-20 w-60" 
                 src="../../public/image/logoproject.png" 
                 alt="Project Manager" />
             </div>
-            <li>Gerenciador</li>
-            <li>Cadastro</li>
-            <li>Sobre</li>
+            <div className="flex space-x-8 mx-auto ">
+            <button className="bg-slate-200 text-black h-10">Gerenciador</button>
+            <button className="bg-slate-200 text-black h-10">Cadastro</button>
+            <button className="bg-slate-200 text-black h-10">Sobre</button>
+            </div>
+            <div>
+              <i className="bi bi-person-circle"></i>
+            </div>
         </nav>
     </div>
   )
