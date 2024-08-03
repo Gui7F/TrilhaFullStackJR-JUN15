9c83d07cf95e9bf047b128cc931912d4b08a4b8c
Guilherme Ribeiro <80535297+Gui7F@users.noreply.github.com>
2024-08-03 19:11:13 -0300

    (raw): add connection on database

--- Project_Manager/backend/API/config/db.js
+++ Project_Manager/backend/API/config/db.js
@@ -1,3 +1,28 @@
 const mongoose = require("mongoose");
 
 //connection
+
+const dbUser = process.env.DB_Password
+const dbPass = process.env.DB_Password
+
+const conn = async () =>{
+
+    try {
+        
+        const dbConnection = await mongoose.connect(
+            `mongodb+srv://${dbUser}:${dbPass}@managercluster.nbvyhmr.mongodb.net/?retryWrites=true&w=majority&appName=managercluster`
+        )
+
+        console.log("Connect on the database")
+
+        return dbConnection
+
+    } catch (error) {
+        
+        console.log(error.message)
+    }
+
+    conn();
+
+    module.exports = conn;
+}
